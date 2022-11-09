54fb77e83716d9a4cd0606c59c3ef53f294782cc
Misung Kim <misung178@gmail.com>
2022-11-10 03:28:26 +0900

    Modified fibonacci one with recursion

--- algorithm-study/baekjoon/10870_fibonacci.c
+++ algorithm-study/baekjoon/10870_fibonacci.c
@@ -6,19 +6,12 @@ https://www.acmicpc.net/problem/10870
 
 int ft_fibonacci(int n)
 {
-    int i;
-    int array[n];
-
-    array[0] = 0;
-    array[1] = 1;
-
-    i = 0;
-    while (i < n - 1)
-    {
-        array[i + 2] = array[i] + array[i + 1];
-        i++;
-    }
-    return (array[i + 1]);
+    if (n == 0)
+        return (0);
+    else if (n == 1)
+        return (1);
+    else
+        return (ft_fibonacci(n - 1) + ft_fibonacci(n - 2));
 }
 
 int main(void)
@@ -27,12 +20,6 @@ int main(void)
 
     num = 0;
     scanf("%d", &num);
-    if (num >= 2)
-        printf("%d", ft_fibonacci(num));
-    else if (num == 1)
-        printf("%d", 1);
-    else if (num == 0)
-        printf("%d", 0);
-
+    printf("%d", ft_fibonacci(num));
     return (0);
 }
